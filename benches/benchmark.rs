ed607f4e807732a1b388f3f8aa51fa41662d2b71
RoDmitry <2397383-RoDmitry@users.noreply.gitlab.com>
2022-08-20 02:52:00 +0300

    Benchmark constants

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,103 +1,115 @@
 use atoi_simd::{parse, parse_i64};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+const LONG_STRING: &str = "1234567890123456";
+const LONG_STRING_NEG: &str = "-1234567890123456";
+const SHORT_STRING: &str = "1";
+const SHORT_STRING_NEG: &str = "-1";
+const FIFTEEN_STRING: &str = "123456789012345";
+const FIFTEEN_STRING_NEG: &str = "-123456789012345";
+
 fn benchmark_long_std(c: &mut Criterion) {
     c.bench_function(
-        "long string std u64                  1234567890123456",
-        |b| b.iter(|| black_box("1234567890123456").parse::<u64>().unwrap()),
+        &("long string std u64                  ".to_owned() + LONG_STRING),
+        |b| b.iter(|| black_box(LONG_STRING).parse::<u64>().unwrap()),
     );
 }
 
 fn benchmark_long_neg_std(c: &mut Criterion) {
     c.bench_function(
-        "long string negative std i64         -1234567890123456",
-        |b| b.iter(|| black_box("-1234567890123456").parse::<i64>().unwrap()),
+        &("long string negative std i64         ".to_owned() + LONG_STRING_NEG),
+        |b| b.iter(|| black_box(LONG_STRING_NEG).parse::<i64>().unwrap()),
     );
 }
 
 fn benchmark_long_u64(c: &mut Criterion) {
     c.bench_function(
-        "long string u64                      1234567890123456",
-        |b| b.iter(|| parse(black_box("1234567890123456")).unwrap()),
+        &("long string u64                      ".to_owned() + LONG_STRING),
+        |b| b.iter(|| parse(black_box(LONG_STRING)).unwrap()),
     );
 }
 
 fn benchmark_long_i64(c: &mut Criterion) {
     c.bench_function(
-        "long string i64                      1234567890123456",
-        |b| b.iter(|| parse_i64(black_box("1234567890123456")).unwrap()),
+        &("long string i64                      ".to_owned() + LONG_STRING),
+        |b| b.iter(|| parse_i64(black_box(LONG_STRING)).unwrap()),
     );
 }
 
 fn benchmark_long_neg_i64(c: &mut Criterion) {
     c.bench_function(
-        "long string negative i64             -1234567890123456",
-        |b| b.iter(|| parse_i64(black_box("-123456789012345")).unwrap()),
+        &("long string negative i64             ".to_owned() + LONG_STRING_NEG),
+        |b| b.iter(|| parse_i64(black_box(LONG_STRING_NEG)).unwrap()),
     );
 }
 
 fn benchmark_short_std(c: &mut Criterion) {
-    c.bench_function("short string std u64                       1", |b| {
-        b.iter(|| black_box("1").parse::<u64>().unwrap())
-    });
+    c.bench_function(
+        &("short string std u64                       ".to_owned() + SHORT_STRING),
+        |b| b.iter(|| black_box(SHORT_STRING).parse::<u64>().unwrap()),
+    );
 }
 
 fn benchmark_short_neg_std(c: &mut Criterion) {
-    c.bench_function("short string negative std i64              -1", |b| {
-        b.iter(|| black_box("-1").parse::<i64>().unwrap())
-    });
+    c.bench_function(
+        &("short string negative std i64              ".to_owned() + SHORT_STRING_NEG),
+        |b| b.iter(|| black_box(SHORT_STRING_NEG).parse::<i64>().unwrap()),
+    );
 }
 
 fn benchmark_short_u64(c: &mut Criterion) {
-    c.bench_function("short string u64                           1", |b| {
-        b.iter(|| parse(black_box("1")).unwrap())
-    });
+    c.bench_function(
+        &("short string u64                           ".to_owned() + SHORT_STRING),
+        |b| b.iter(|| parse(black_box(SHORT_STRING)).unwrap()),
+    );
 }
 
 fn benchmark_short_i64(c: &mut Criterion) {
-    c.bench_function("short string i64                           1", |b| {
-        b.iter(|| parse_i64(black_box("1")).unwrap())
-    });
+    c.bench_function(
+        &("short string i64                           ".to_owned() + SHORT_STRING),
+        |b| b.iter(|| parse_i64(black_box(SHORT_STRING)).unwrap()),
+    );
 }
 
 fn benchmark_short_neg_i64(c: &mut Criterion) {
-    c.bench_function("short string negative i64                  -1", |b| {
-        b.iter(|| parse_i64(black_box("-1")).unwrap())
-    });
+    c.bench_function(
+        &("short string negative i64                  ".to_owned() + SHORT_STRING_NEG),
+        |b| b.iter(|| parse_i64(black_box(SHORT_STRING_NEG)).unwrap()),
+    );
 }
 
 fn benchmark_long15_std(c: &mut Criterion) {
     c.bench_function(
-        "15 chars string std u64              123456789012345",
-        |b| b.iter(|| black_box("123456789012345").parse::<u64>().unwrap()),
+        &("15 chars string std u64              ".to_owned() + FIFTEEN_STRING),
+        |b| b.iter(|| black_box(FIFTEEN_STRING).parse::<u64>().unwrap()),
     );
 }
 
 fn benchmark_long15_neg_std(c: &mut Criterion) {
     c.bench_function(
-        "15 chars string negative std i64     -123456789012345",
-        |b| b.iter(|| black_box("-123456789012345").parse::<i64>().unwrap()),
+        &("15 chars string negative std i64     ".to_owned() + FIFTEEN_STRING_NEG),
+        |b| b.iter(|| black_box(FIFTEEN_STRING_NEG).parse::<i64>().unwrap()),
     );
 }
 
 fn benchmark_long15_u64(c: &mut Criterion) {
     c.bench_function(
-        "15 chars string u64                  123456789012345",
-        |b| b.iter(|| parse(black_box("123456789012345")).unwrap()),
+        &("15 chars string u64                  ".to_owned() + FIFTEEN_STRING),
+        |b| b.iter(|| parse(black_box(FIFTEEN_STRING)).unwrap()),
     );
 }
 
 fn benchmark_long15_i64(c: &mut Criterion) {
     c.bench_function(
-        "15 chars string i64                  123456789012345",
-        |b| b.iter(|| parse_i64(black_box("123456789012345")).unwrap()),
+        &("15 chars string i64                  ".to_owned() + FIFTEEN_STRING),
+        |b| b.iter(|| parse_i64(black_box(FIFTEEN_STRING)).unwrap()),
     );
 }
 
 fn benchmark_long15_neg_i64(c: &mut Criterion) {
     c.bench_function(
-        "15 chars string negative i64         -123456789012345",
-        |b| b.iter(|| parse_i64(black_box("-123456789012345")).unwrap()),
+        &("15 chars string negative i64         ".to_owned() + FIFTEEN_STRING_NEG),
+        |b| b.iter(|| parse_i64(black_box(FIFTEEN_STRING_NEG)).unwrap()),
     );
 }
 
