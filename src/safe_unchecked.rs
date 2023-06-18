0daf811a7e73f3c494ac97615f8b41b871fd5d4f
RoDmitry <gh@rdmtr.com>
2023-06-18 13:01:54 +0300

    Refactor

--- src/safe_unchecked.rs
+++ src/safe_unchecked.rs
@@ -1,16 +1,16 @@
 use core::slice::SliceIndex;
 
 pub(crate) trait SliceGetter<T> {
-    fn get_safe_unchecked<I>(&self, index: I) -> &<I as SliceIndex<[T]>>::Output
+    fn get_safe_unchecked<I>(&self, index: I) -> &<I>::Output
     where
-        I: SliceIndex<[T]>;
+        I: SliceIndex<Self>;
 }
 
 impl<T> SliceGetter<T> for [T] {
     #[inline(always)]
-    fn get_safe_unchecked<I>(&self, index: I) -> &<I as SliceIndex<[T]>>::Output
+    fn get_safe_unchecked<I>(&self, index: I) -> &<I>::Output
     where
-        I: SliceIndex<[T]>,
+        I: SliceIndex<Self>,
     {
         if cfg!(debug_assertions) {
             &self[index]
