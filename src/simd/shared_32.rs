4bdebd362fc9ea628da5a5ba373c8f3efec4fc37
RoDmitry <gh@rdmtr.com>
2025-12-10 03:14:07 +0400

    Fix SSE_AVX leading zeroes

--- src/simd/shared_32.rs
+++ src/simd/shared_32.rs
@@ -8,7 +8,7 @@ fn parse_simd_checked_pre_pos(s: &[u8]) -> Result<u64, AtoiSimdError<'_>> {
     let (res, len) = if s.len() < super::SHORT {
         parse_short_pos::<{ u64::MAX }>(s)
     } else {
-        super::parse_simd_16(s)
+        super::parse_simd_16_skipped(s)
     }?;
     if len != s.len() {
         return Err(AtoiSimdError::Invalid64(res, len, s));
@@ -21,7 +21,7 @@ fn parse_simd_checked_pre_neg(s: &[u8]) -> Result<i64, AtoiSimdError<'_>> {
     let (res, len) = if s.len() < super::SHORT {
         parse_short_neg::<{ i64::MIN }>(s)
     } else {
-        super::parse_simd_16(s).map(|(v, l)| (-(v as i64), l))
+        super::parse_simd_16_skipped(s).map(|(v, l)| (-(v as i64), l))
     }?;
     if len != s.len() {
         return Err(AtoiSimdError::Invalid64(-res as u64, len, s));
@@ -31,7 +31,7 @@ fn parse_simd_checked_pre_neg(s: &[u8]) -> Result<i64, AtoiSimdError<'_>> {
 
 #[inline(always)]
 pub(crate) fn parse_simd<const MAX: u64>(s: &[u8]) -> Result<(u64, usize), AtoiSimdError<'_>> {
-    let (res, len) = super::parse_simd_16(s)?;
+    let (res, len) = super::parse_simd_16_skipped(s)?;
     if res > MAX {
         Err(AtoiSimdError::Overflow(s))
     } else {
@@ -52,7 +52,7 @@ pub(crate) fn parse_simd_checked<const MAX: u64>(s: &[u8]) -> Result<u64, AtoiSi
 #[inline(always)]
 pub(crate) fn parse_simd_neg<const MIN: i64>(s: &[u8]) -> Result<(i64, usize), AtoiSimdError<'_>> {
     debug_assert!(MIN < 0);
-    let (res, len) = super::parse_simd_16(s)?;
+    let (res, len) = super::parse_simd_16_skipped(s)?;
     let min = -MIN as u64;
     if res > min {
         Err(AtoiSimdError::Overflow(s))
